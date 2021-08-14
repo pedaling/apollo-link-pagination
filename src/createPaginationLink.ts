43ae17a7f6e6eb27001333f3d77a7bad6a160419
peter <tbvjaos510@naver.com>
2021-08-15 08:02:55 +0900

    fix: support tree shaking (#1)
    * fix: support tree shaking
    
    * fix: revert change
    
    * fix: change to yarn

--- src/createPaginationLink.ts
+++ src/createPaginationLink.ts
@@ -5,32 +5,50 @@ import get from 'lodash.get';
 
 const appliedPolicies: string[] = [];
 
-export function createPaginationLink(cache: InMemoryCache, paginatePolicies: Record<string, FieldPolicy>): ApolloLink {
+export function createPaginationLink(
+  cache: InMemoryCache,
+  paginatePolicies: Record<string, FieldPolicy>
+): ApolloLink {
   return new ApolloLink((operation, forward) => {
     const { query } = operation;
     const possibleDirectives = Object.keys(paginatePolicies);
 
     visit(query, {
       Directive(node, _key, _parent, path) {
-        const foundDirective = possibleDirectives.find((directive) => directive === node.name.value);
+        const foundDirective = possibleDirectives.find(
+          directive => directive === node.name.value
+        );
 
         if (!foundDirective) {
           return;
         }
 
         // Clean Query
-        operation.query = removeDirectivesFromDocument([{ name: foundDirective }], query) ?? query;
+        operation.query =
+          removeDirectivesFromDocument([{ name: foundDirective }], query) ??
+          query;
 
-        const field = get(query, path.join('.').replace(/\.directives\.0$/, ''));
+        const field = get(
+          query,
+          path.join('.').replace(/\.directives\.0$/, '')
+        );
 
-        const typenameArg = node.arguments?.find((arg) => arg.name.value === 'typename');
-        const typename = (typenameArg?.value as StringValueNode)?.value ?? 'Query';
+        const typenameArg = node.arguments?.find(
+          arg => arg.name.value === 'typename'
+        );
+        const typename =
+          (typenameArg?.value as StringValueNode)?.value ?? 'Query';
 
-        const keyArgsArg = node.arguments?.find((arg) => arg.name.value === 'keyArgs');
-        const keyArgs = (keyArgsArg?.value as ListValueNode)?.values.map((v) => (v as StringValueNode).value) ?? [];
+        const keyArgsArg = node.arguments?.find(
+          arg => arg.name.value === 'keyArgs'
+        );
+        const keyArgs =
+          (keyArgsArg?.value as ListValueNode)?.values.map(
+            v => (v as StringValueNode).value
+          ) ?? [];
 
         // Check if the policies are already applied
-        const key = `${typename}-${field.name.value}`
+        const key = `${typename}-${field.name.value}`;
         if (appliedPolicies.includes(key)) {
           return;
         }
